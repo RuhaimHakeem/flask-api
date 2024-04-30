d0e2df6caef2b101468a4f54e37bbf04886107d2
Ruhaim <ruhaim860@gmail.com>
2024-05-01 00:20:25 +0530

    Minor changes

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 app = Flask(__name__)
 
-model = './sm-bert-model'
-tokenizer = './tokenizer'
-
 tokenizer = AutoTokenizer.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
 
 model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-multilingual-uncased-sentiment')
@@ -14,17 +11,16 @@ model = AutoModelForSequenceClassification.from_pretrained('nlptown/bert-base-mu
 def hello():
   return "Hello World!"
 
-
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
     text = data['text']
+
     tokens = tokenizer.encode(text, return_tensors='pt')
     result = model(tokens)
     sentiment = int(torch.argmax(result.logits))+1
 
     response = jsonify({'sentiment': sentiment})
-    
     return response
 
 if __name__ == "__main__":
